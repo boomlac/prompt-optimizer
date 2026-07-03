4feb1eb523000634254c7bcae97d4f8152787243
Sumit Shridhar <sumit.s.kumbhar@gmail.com>
2026-07-02 18:17:46 -0700

    Add Firebase Hosting deployment workflow and configure Firebase in environment files

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,27 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, PLATFORM_ID, inject, provideAppInitializer, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { apiKeyInterceptor } from './core/interceptors/api-key.interceptor';
+import { environment } from '../environments/environment.development';
+import { initializeApp } from 'firebase/app';
+import { isPlatformBrowser } from '@angular/common';
+import { getAnalytics } from 'firebase/analytics';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-     provideHttpClient(withInterceptors([apiKeyInterceptor])),
+    provideHttpClient(withInterceptors([apiKeyInterceptor])),
     provideClientHydration(withEventReplay()),
+      provideAppInitializer(() => {
+      const platformId = inject(PLATFORM_ID);
+      if (isPlatformBrowser(platformId)) {
+        const app = initializeApp(environment.firebaseConfig);
+        getAnalytics(app);
+      }
+    }),
   ]
 };
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,14 @@
 export const environment = {
-  production: true,
-promptAnalysisApiUrl: ' https://us-central1-examatics-api-384f7.cloudfunctions.net/promptOptimizer/api/v1/ai/prompt-analysis',
-     apiKey: 'dev-api-key',
+  production: false,
+  promptAnalysisApiUrl: ' https://us-central1-examatics-api-384f7.cloudfunctions.net/promptOptimizer/api/v1/ai/prompt-analysis',
+  firebaseConfig: {
+    apiKey: "AIzaSyA5v79e5IOv2otYR1nqa0yBcKyjF-MCD78",
+    authDomain: "societyloops.firebaseapp.com",
+    databaseURL: "https://societyloops.firebaseio.com",
+    projectId: "societyloops",
+    storageBucket: "societyloops.appspot.com",
+    messagingSenderId: "81257901428",
+    appId: "1:81257901428:web:69eb70934a697d7461d021",
+    measurementId: "G-G3GCZVPZ66"
+  }
 };
